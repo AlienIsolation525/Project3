0771ee6658249629f0e9f56d553c3be08804b431
AlienIsolation525 <vitrage091@gmail.com>
2024-12-10 08:13:10 +0300

    logger fix

--- Logger.cpp
+++ Logger.cpp
@@ -2,9 +2,39 @@
 
 void Logger::save_message_log(std::string s, std::string sender, std::string receiver) {
 
-	std::ofstream out(filename, std::ios::app);
-	out << std::endl << count << ".  " << "user " << sender << " say '" << s
-		<< "' to user " << receiver;
-	count++;
-	out.close();
+    if (file.is_open())
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        file << (countLines() + 1) << ".  " << "user " << sender << " say '" << s
+            << "' to user " << receiver << std::endl;
+    }
 };
+
+std::string Logger::readLine() {
+    std::ifstream file(filename);
+    std::string line;
+
+    if(file.is_open())
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        std::getline(file, line);
+    }
+
+    return line;
+}
+
+int Logger::countLines() {
+    std::ifstream file(filename);
+    std::string str;
+    int res = 0;
+
+    if (file.is_open())
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        for (std::string line; getline(file, str); ) {
+            res++;
+        }
+    }
+
+    return res;
+}
--- Logger.h
+++ Logger.h
@@ -1,16 +1,31 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <mutex>
 #define  filename "log.txt"
 
 class Logger {
 public:
-    Logger() = default;
-    ~Logger() = default;
+    Logger() {
+        file.open(filename, std::ios_base::app);
+    }
+    ~Logger() {
+        file.close();
+    }
 
-    //Save message to log(sender_id, message text, receiver id)
+    // Save message to log(sender_id, message text, receiver id)
     void save_message_log(std::string s, std::string sender, std::string receiver);
+
+    // Read line from log
+    std::string readLine();
+
+    // Return number of lines in log file
+    int countLines();
 
 private:
     int count = 1;
+    std::mutex mutex;
+    std::ofstream file;
+    std::string line;
 };
